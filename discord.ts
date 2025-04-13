75976c790b3ad07b0c5f9265065db4744ea250cb
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-04-14 00:30:54 +0900

    スレッド削除機能を削除

--- discord.ts
+++ discord.ts
@@ -58,41 +58,39 @@ discord.on("interactionCreate", async (interaction) => {
   if (!interaction.isButton()) return
   if (!interaction.message.author.bot) return
   const mode = interaction.customId.split("/")[0]
-
-  if (mode === "send") {
-    const userId = interaction.customId.split("/")[1]
-    const replyMessageId = interaction.message.reference?.messageId
-    const replyMessage = replyMessageId && interaction.channel?.messages.cache.get(replyMessageId)
-    if (!replyMessage) {
-      interaction.reply("送信するメッセージの内容を取得できません。")
+  switch (mode) {
+    case "send": {
+      const userId = interaction.customId.split("/")[1]
+      const replyMessageId = interaction.message.reference?.messageId
+      const replyMessage = replyMessageId && interaction.channel?.messages.cache.get(replyMessageId)
+      if (!replyMessage) {
+        interaction.reply("送信するメッセージの内容を取得できません。")
+        return
+      }
+      if (!userId) {
+        await replyMessage.reply({
+          content: "送信先ユーザーを取得できません。",
+        })
+        return
+      }
+      const messageContent = getMessageContent(replyMessage)
+      const attachments = replyMessage.attachments
+      try {
+        await send(messageContent, userId, attachments)
+        await replyMessage.reply({
+          content: "送信しました",
+        })
+      } catch (e) {
+        await replyMessage.reply({
+          content: `メッセージを送信できません: ${(e as Error).message ?? "不明なエラー"}`,
+        })
+      }
+      await interaction.message.delete()
       return
     }
-    if (!userId) {
-      await replyMessage.reply({
-        content: "送信先ユーザーを取得できません。",
-      })
-      return
-    }
-    const messageContent = getMessageContent(replyMessage)
-    const attachments = replyMessage.attachments
-    try {
-      await send(messageContent, userId, attachments)
-      await replyMessage.reply({
-        content: "送信しました",
-      })
-    } catch (e) {
-      await replyMessage.reply({
-        content: `メッセージを送信できません: ${(e as Error).message ?? "不明なエラー"}`,
-      })
-    }
-    await interaction.message.delete()
-    return
-  } else if (mode === "close") {
-    if (interaction.channel?.isThread()) {
-      await interaction.channel.delete()
-    }
-  } else if (mode === "delete") {
-    await interaction.message.delete()
+    case "delete":
+      await interaction.message.delete()
+      break
   }
 })
 
@@ -111,17 +109,6 @@ export async function createThreadAndSendMessages(userId: string, messages: stri
     type: ChannelType.PublicThread,
   })
   await ThreadDB.create({ userId: userId, threadId: created.id })
-
-  const button = new ButtonBuilder()
-    .setCustomId(`close`)
-    .setLabel("削除")
-    .setStyle(ButtonStyle.Danger)
-  const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button)
-
-  await created.send({
-    content: "スレッドを削除する。",
-    components: [row],
-  })
 
   for (const message of messages) {
     await created.send(message)
